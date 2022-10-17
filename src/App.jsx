9f39bad033dd32c0ef1eecf30da504464fa2be3e
Khaldoun Halawani <kldonnn@gmail.com>
2022-10-17 20:11:40 +0300

    Create Todo app input form

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Form from './components/form/form.component';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>R-ToDOApp</h1>
+      <Form />
+    </div>
+  );
+}
+
+export default App;
