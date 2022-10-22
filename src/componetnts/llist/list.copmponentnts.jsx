5051274d21205771b5b7ed3940986ad4a0d001ed
MohammadRajabi <21910398@students.hebron.edu>
2022-10-22 11:03:22 +0300

    style new

--- src/componetnts/llist/list.copmponentnts.jsx
+++ src/componetnts/llist/list.copmponentnts.jsx
@@ -9,24 +9,27 @@ import './list.css';
  * }} props 
  */
 function List(props) {
-  return (
-    <div className='items_list'>
-      <h1>The Items List</h1>
-      {
-        /* <ul>
-              {
-                  props.items.map(item=><li>{item.title}-{item.category}-{item.urgent===true?("urgent:Urgent"):"urgent:NotUrgent"}</li>)
-              }
-            </ul> */
-      }
-      <div className='licenter'>
-        <ol className="list">
-          {
-            props.items.map((item, index) => <li key={index} className="list-item one"> <span style={index % 2 === 0 ? { backgroundColor: "cadetblue" } : { backgroundColor: "rgb(255, 68, 69)" }}>{item.title}-{item.category}-{item.urgent ? ("urgent:Urgent") : "urgent:NotUrgent"}</span></li>)
-          }
-        </ol>
-      </div>
-    </div>
-  )
+    return (
+        <div className='items_list'>
+            <h1>The Items List</h1>
+            {/* <ul>
+            {
+                props.items.map(item=><li>{item.title}-{item.category}-{item.urgent===true?("urgent:Urgent"):"urgent:NotUrgent"}</li>)
+            }
+          </ul> */}
+            <div className='licenter'>
+            <ol className="list">
+                {
+                    props.items.map((item, index) => <li  key={index} className="list-item one"> <span style={index%2===0?{backgroundColor:"cadetblue"}:{backgroundColor:"rgb(255, 68, 69)"}}>{item.title}-{item.category}-{item.urgent ? ("urgent:Urgent") : "urgent:NotUrgent"}</span></li>)
+                }
+
+
+            </ol>
+            </div>
+
+
+
+        </div>
+    )
 }
 export default List;
