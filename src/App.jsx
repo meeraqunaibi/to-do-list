dc2e2572dab6bc128e5bea6599cd9d016cb80f89
Khaldoun Halawani <kldonnn@gmail.com>
2022-10-22 10:57:06 +0300

    Fix imports

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import Form from "./components/form/form.components";
-import "./App.css"
 import { useState } from "react";
-import List from "./components/list/list.components";
+import Form from "./components/form/form.component";
+import "./App.css"
+import List from "./components/list/list.component";
+
 function App() {
-  const [items,setItems] = useState([]);
-  const addItem =(item)=>{
-    console.log("add item" ,item);
-    setItems([...items,item])
+  const [items, setItems] = useState([]);
+  const addItem = (item) => {
+    console.log("add item", item);
+    setItems([...items, item])
   }
   return (
     <div className="App">
       <h1>To Do List</h1>
-      <Form onAddItem ={addItem}/>
-      <List items={items}/>
+      <Form onAddItem={addItem} />
+      <List items={items} />
     </div>
   );
 }
--- src/components/form/form.component.jsx
+++ src/components/form/form.component.jsx
@@ -1,50 +1,50 @@
 import { useState } from "react";
 import "./form.css";
 const Form = (props) => {
-    const [title , setTitle]=useState("Do homework");
-    const [ category , setCategory]=useState("");
-    const [urgent , setUrgent]=useState(false);
-    const handelSubmit=(e)=>{
-     e.preventDefault();
-     const newItem = {
-        title : e.target.title.value , 
-        category : e.target.category.value ,
-        urgent : e.target.urgent.checked
-     }
-     props.onAddItem(newItem);
-     setTitle("");
-     setCategory("");
-     setUrgent(false);
+    const [title, setTitle] = useState("Do homework");
+    const [category, setCategory] = useState("");
+    const [urgent, setUrgent] = useState(false);
+    const handelSubmit = (e) => {
+        e.preventDefault();
+        const newItem = {
+            title: e.target.title.value,
+            category: e.target.category.value,
+            urgent: e.target.urgent.checked
+        }
+        props.onAddItem(newItem);
+        setTitle("");
+        setCategory("");
+        setUrgent(false);
     }
-    const handelTitle=(e)=>{
-        let t = e.target.value ; 
-        t = t.replace (".","-");
+    const handelTitle = (e) => {
+        let t = e.target.value;
+        t = t.replace(".", "-");
         setTitle(t);
     }
-  return (
-    <div className="container">
-      <form onSubmit={handelSubmit}>
-        <input 
-        type="text"
-         placeholder="tittle"
-         name="title"
-         value={title}
-         onChange = {handelTitle}
-         />
-        <select name="category" value={category} 
-        onChange={(e)=>setCategory(e.target.value)}>
-        <option  value="select">select</option>
-          <option value="work">work</option>
-          <option value="home">home</option>
-          <option value="personal">personal</option>
-        </select>
-        <input name="urgent" type="checkbox" checked={urgent}
-         onChange={(e)=>setUrgent(e.target.value)}></input>
-        <label className="label">Urgent</label>
-        <input type="submit" value="add to list"/>
-      </form>
-      <hr></hr>
-    </div>
-  );
+    return (
+        <div className="container">
+            <form onSubmit={handelSubmit}>
+                <input
+                    type="text"
+                    placeholder="tittle"
+                    name="title"
+                    value={title}
+                    onChange={handelTitle}
+                />
+                <select name="category" value={category}
+                    onChange={(e) => setCategory(e.target.value)}>
+                    <option value="select">select</option>
+                    <option value="work">work</option>
+                    <option value="home">home</option>
+                    <option value="personal">personal</option>
+                </select>
+                <input name="urgent" type="checkbox" checked={urgent}
+                    onChange={(e) => setUrgent(e.target.value)}></input>
+                <label className="label">Urgent</label>
+                <input type="submit" value="add to list" />
+            </form>
+            <hr></hr>
+        </div>
+    );
 };
 export default Form;
