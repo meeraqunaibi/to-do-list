2b8e234f3b2accdeeaf75b112a30ed49203106a1
mahmoudharb105 <mahmoudharb105@gmail.com>
2022-10-22 12:31:56 +0300

    add

--- src/components/category/catedory.js
+++ src/components/category/catedory.js
@@ -2,8 +2,8 @@ const category_items = [
     { value: "home" ,label: "Home"},
     { value: "personal" ,label: "Personal"},
     { value: "work" ,label: "Work"},
-    { value: "University" ,label: "Work"},
-    { value: "friend" ,label: "Work"}
+    { value: "University" ,label: "University"},
+    { value: "friend" ,label: "friend"}
 ];
 
 export default category_items;
