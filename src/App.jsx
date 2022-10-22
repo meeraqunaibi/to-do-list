119dddfca8b73d7754a34bd9549ac3028dc05508
Moawiyah <moawiyah.nat@gmail.com>
2022-10-22 10:41:10 +0300

    change app name

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState } from 'react';
-import './App.css';
-import Form from './components/form/form.component';
-import List from './components/list/list.component';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/form/form.component";
+import List from "./components/list/list.component";
 
 function App() {
   const [items, setItems] = useState([]);
 
   const addItem = (item) => {
     setItems([...items, item]);
-  }
+  };
 
   return (
     <div className="App">
-      <h1>R-ToDOApp</h1>
+      <h1>Happy Hacking!-ToDOApp</h1>
       <Form onAddItem={addItem} />
       <List items={items} />
     </div>
