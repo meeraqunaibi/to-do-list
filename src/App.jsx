74583ae5cc81c6f7fea055855341741e99a0c8a0
Meera Qunaibe <191128@ppu.edu.ps>
2022-10-26 11:59:32 +0300

    rendering-addPage-if-itemsArray-is-empty

--- src/App.jsx
+++ src/App.jsx
@@ -5,25 +5,32 @@ import AddItem from "./pages/addItem/addItem.component";
 import ViewItems from "./pages/viewItems/viewItem.component";
 
 function App() {
-  const [items, setItems] = useState([]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("to-do-list")) || []
+  );
   const [isShown, setIsShown] = useState(false);
   const [id, setId] = useState();
-  const [currentPage, setCurrentPage] = useState("");
+  const [currentPage, setCurrentPage] = useState(
+    items.length === 0 ? "add" : "view"
+  );
   const addNewItem = (item) => {
     let newItems = [...items, item];
     setItems(newItems);
     localStorage.setItem("to-do-list", JSON.stringify(newItems));
+    setCurrentPage("view");
   };
   const onDelete = (id) => {
     const newItems = items.filter((item) => item.id !== id);
     setItems(newItems);
     setIsShown(false);
+    localStorage.setItem("to-do-list", JSON.stringify(newItems));
   };
   const onFinish = (id) => {
     const newItems = items.map((item) =>
       item.id === id ? { ...item, isDone: true } : item
     );
     setItems(newItems);
+    localStorage.setItem("to-do-list", JSON.stringify(newItems));
   };
 
   const undoDelete = () => {
