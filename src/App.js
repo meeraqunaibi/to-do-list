92c043155c290bb26d477b09de9c897d6a7b92ac
MohammadRajabi <21910398@students.hebron.edu>
2022-10-31 17:14:28 +0200

    last_update_for_to_do_app

--- src/App.js
+++ src/App.js
@@ -1,31 +1,33 @@
+import logo from './logo.svg';
 import './App.css';
 import Form from './componetnts/form/form.componetnts';
 import { useState } from 'react';
 import List from './componetnts/llist/list.copmponentnts';
-
+import Popup from './componetnts/popup/popup.componetnt';
 function App() {
-  const initialItems = JSON.parse(localStorage.getItem("todolist"));
-
-  const [item, setItem] = useState(initialItems);
-  const [Page, setPage] = useState(initialItems.length > 0 ? "View" : "Add");
-
+  const [item, setItem] = useState(JSON.parse(localStorage.getItem("todolist")) || []);
+  const [Page, setPage] = useState("View");
   const addItem = (itemm) => {
     const newitem = [...item, itemm];
     setItem(newitem);
     localStorage.setItem('todolist', JSON.stringify(newitem));
+
   }
 
   const Delete = (id) => {
     const newItems = item.filter(item => item.id !== id);
     setItem(newItems);
     localStorage.setItem('todolist', JSON.stringify(newItems));
-  }
 
+  }
   const Checked = (id) => {
     const newItems = item.map(item => item.id === id ? { ...item, checked: true } : item);
     setItem(newItems)
     localStorage.setItem('todolist', JSON.stringify(newItems));
   }
+
+
+
 
   return (
 
@@ -33,13 +35,19 @@ function App() {
       <div className='nav'>
         <h1 style={{ color: "black" }}>R-To Do App</h1>
         <div>
-          <button style={Page === "Add" ? { boxShadow: "2px 2px 2px 2px tomato " } : null} onClick={() => setPage("Add")} id='add' >Add Task</button>
-          <button style={Page === "View" ? { boxShadow: "2px 2px 2px 2px skyblue" } : null} onClick={() => setPage("View")} id='view' >View Task</button>
+          <button style={Page == "Add" ? { boxShadow: "2px 2px 2px 2px tomato " } : null} onClick={() => setPage("Add")} id='add' >Add Task</button>
+          <button style={Page == "View" ? { boxShadow: "2px 2px 2px 2px skyblue" } : null} onClick={() => setPage("View")} id='view' >View Task</button>
         </div>
       </div>
       {
-        Page === "View" ? <List onDelete={Delete} onCheck={Checked} items={item} /> : <Form onAddFunction={addItem} />
+        Page == "View" && (item.length) ? <List onDelete={Delete} onCheck={Checked} items={item} /> : <Form onAddFunction={addItem} />
       }
+      {
+        console.log(JSON.stringify(item).length)
+      }
+
+
+
 
     </div>
   );
--- src/componetnts/llist/list.copmponentnts.jsx
+++ src/componetnts/llist/list.copmponentnts.jsx
@@ -1,5 +1,5 @@
 import './list.css';
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import ReactDOM from 'react-dom/client';
 import Category_option from '../form/data';
 import Popup from '../popup/popup.componetnt';
@@ -16,36 +16,40 @@ import Popup from '../popup/popup.componetnt';
 function List(props) {
   const [isOpen, SetIsOpen] = useState(false);
   const [isSure, setIsSure] = useState(false);
+  const [currentId, setCurrentId] = useState(-1);
+
   const toggleSure = () => {
     console.log("hello")
     setIsSure(!isSure);
+    props.onDelete(currentId);
+    setIsSure(false);
   }
+
+  useEffect(() => {
+    setTimeout(
+      () => {
+        console.log("hello");
+      }
+      , 1000
+    )
+    return () => {
+      console.log("bye");
+
+
+    }
+
+
+  }, []);
 
   const toggleOpen = () => {
     console.log("hello")
     SetIsOpen(!isOpen);
   }
 
-  function myFunction(id) {
-    if (window.confirm("are you sure") === true) {
-      props.onDelete(id);
-
-    } else {
-
-    }
-    // <Popup onPopup={props.onPopup}></Popup>
-    //    toggleOpen();
-    //    if(isSure===true)
-    //     {
-    //      props.onDelete(id);
-    //      toggleOpen();
-    //     }
-
-
-  }
-  function compareFn(a) {
-    if (a === true) {
-      return a;
+  function CheckedToDo(id) {
+    toggleOpen();
+    if (isSure === true) {
+      toggleOpen();
     }
   }
   return (
@@ -53,57 +57,99 @@ function List(props) {
 
 
 
-      {/* {
-        isOpen?(<Popup onPopup={toggleOpen} onSure={toggleSure}></Popup>):null
-         } */}
+      {
+        isOpen ? (<div className='mainpop'><Popup onPopup={toggleOpen} onSure={toggleSure}></Popup></div>) : null
+      }
 
       <h1>The Items List</h1>
       <div className='root'></div>
-      {/* <ul>
-            {
-                props.items.map(item=><li>{item.title}-{item.category}-{item.urgent===true?("urgent:Urgent"):"urgent:NotUrgent"}</li>)
-            }
-          </ul> */}
+        <ol className="list">
 
-      <ol className="list">
+          {
 
-        {
-
-          props.items.map((item, index) => (
+            props.items.map((item, index) => (
+              !item.checked ? (
 
 
-            <li key={item.title + item.category + index} >
-              <div style={
+                <li key={item.title + item.category + index} >
+                  <div style={
 
-                item.category === "home" && item.urgent === true ? { backgroundColor: "#4C3A51", borderLeft: "12px solid green" }
-                  : item.category === "home" && item.urgent === false ? { backgroundColor: "#4C3A51", borderLeft: "12px solid red" }
-                    : item.category === "work" && item.urgent === true ? { backgroundColor: "#B25068", borderLeft: "12px solid green" }
-                      : item.category === "work" && item.urgent === false ? { backgroundColor: "#B25068", borderLeft: "12px solid red" }
-                        : item.category === "school" && item.urgent === true ? { backgroundColor: "#774360", borderLeft: "12px solid green" }
-                          : item.category === "school" && item.urgent === false ? { backgroundColor: "#774360", borderLeft: "12px solid red" }
-                            : null
-
-
-              } className='Todo_card'>
-                <div className='Todo_info'>
-                  <h2 style={item.checked ? { textDecorationLine: "line-through" } : null} >{item.title}</h2>
-                  <p style={item.checked ? { textDecorationLine: "line-through" } : null}>{item.category}</p>
-                </div>
-
-                <div className='Button_todo'>
-                  <button onClick={
-                    () => myFunction(item.id) /*()=>props.onDelete(item.id)*/} id='false'>&#10008;</button>
-                  <button disabled={item.checked ? true : false} style={item.checked ? { pointerEvents: "mouse", cursor: "not-allowed", transform: "none" } : null} onClick={() => (props.onCheck(item.id))} id='true'>&#10003;</button>
-                </div>
+                    item.category === "home" && item.urgent === true ? { backgroundColor: "#4C3A51", borderLeft: "12px solid green" }
+                      : item.category === "home" && item.urgent === false ? { backgroundColor: "#4C3A51", borderLeft: "12px solid red" }
+                        : item.category === "work" && item.urgent === true ? { backgroundColor: "#B25068", borderLeft: "12px solid green" }
+                          : item.category === "work" && item.urgent === false ? { backgroundColor: "#B25068", borderLeft: "12px solid red" }
+                            : item.category === "school" && item.urgent === true ? { backgroundColor: "#774360", borderLeft: "12px solid green" }
+                              : item.category === "school" && item.urgent === false ? { backgroundColor: "#774360", borderLeft: "12px solid red" }
+                                : null
 
 
-              </div>
-            </li>
+                  } className='Todo_card'>
+                    <div className='Todo_info'>
+                      <h2 style={item.checked ? { textDecorationLine: "line-through" } : null} >{item.title}</h2>
+                      <p style={item.checked ? { textDecorationLine: "line-through" } : null}>{item.category}</p>
+                    </div>
+
+                    <div className='Button_todo'>
+                      <button onClick={
+                        () => {
+                          setCurrentId(item.id)
+                          CheckedToDo(item.id) /*()=>props.onDelete(item.id)*/
+                        }
+                      } id='false'>&#10008;</button>
+                      <button disabled={item.checked ? true : false} style={item.checked ? { pointerEvents: "mouse", cursor: "not-allowed", transform: "none" } : null} onClick={() => (props.onCheck(item.id))} id='true'>&#10003;</button>
+                    </div>
 
 
-          ))
-        }
-      </ol>
+                  </div>
+                </li>) : null
+
+
+
+            ))
+          }
+            {/* checked to do show last */}
+          {
+            props.items.map((item, index) => (
+              item.checked ? (
+
+
+                <li key={item.title + item.category + index} >
+                  <div style={
+
+                    item.category === "home" && item.urgent === true ? { backgroundColor: "#4C3A51", borderLeft: "12px solid green" }
+                      : item.category === "home" && item.urgent === false ? { backgroundColor: "#4C3A51", borderLeft: "12px solid red" }
+                        : item.category === "work" && item.urgent === true ? { backgroundColor: "#B25068", borderLeft: "12px solid green" }
+                          : item.category === "work" && item.urgent === false ? { backgroundColor: "#B25068", borderLeft: "12px solid red" }
+                            : item.category === "school" && item.urgent === true ? { backgroundColor: "#774360", borderLeft: "12px solid green" }
+                              : item.category === "school" && item.urgent === false ? { backgroundColor: "#774360", borderLeft: "12px solid red" }
+                                : null
+
+
+                  } className='Todo_card'>
+                    <div className='Todo_info'>
+                      <h2 style={item.checked ? { textDecorationLine: "line-through" } : null} >{item.title}</h2>
+                      <p style={item.checked ? { textDecorationLine: "line-through" } : null}>{item.category}</p>
+                    </div>
+
+                    <div className='Button_todo'>
+                      <button onClick={
+                        () => {
+                          setCurrentId(item.id)
+                          CheckedToDo(item.id) /*()=>props.onDelete(item.id)*/
+                        }
+                      } id='false'>&#10008;</button>
+                      <button disabled={item.checked ? true : false} style={item.checked ? { pointerEvents: "mouse", cursor: "not-allowed", transform: "none" } : null} onClick={() => (props.onCheck(item.id))} id='true'>&#10003;</button>
+                    </div>
+
+
+                  </div>
+                </li>) : null
+
+
+
+            ))
+          }
+        </ol>
     </div>
   )
 }
