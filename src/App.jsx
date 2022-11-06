69377efc6a1917dc28b3fed6d754f87e5411d121
aseel amleh <181038@ppu.edu.ps>
2022-11-06 12:58:56 +0200

    show dialog done

--- src/App.jsx
+++ src/App.jsx
@@ -1,52 +1,82 @@
 import { useState } from "react";
 import Form from "./components/form/form.component";
-import "./App.css"
+import "./App.css";
 import List from "./components/list/list.component";
 import AddItemPage from "./components/pages/addItem/addItem.component";
 import ViewItemPage from "./components/pages/viewItem/viewItem.component";
+import Dialog from "./dialog/dialog.components";
 
 function App() {
-  const [items, setItems] = useState(JSON.parse(localStorage.getItem('ToDo'))||[]);
+  const [items, setItems] = useState(
+    JSON.parse(localStorage.getItem("ToDo")) || []
+  );
   const [currentPage, setCurrentPage] = useState([]);
+  const [item, setItem] = useState(undefined);
   const addItem = (item) => {
     console.log("add item", item);
-    if (item.urgent ){
-      const newItem = [item,...items];
+    if (item.urgent) {
+      const newItem = [item, ...items];
       setItems(newItem);
-      localStorage.setItem( 'ToDo' , JSON.stringify(newItem));
+      localStorage.setItem("ToDo", JSON.stringify(newItem));
+    } else {
+      const newItem = [...items, item];
+      setItems(newItem);
+      localStorage.setItem("ToDo", JSON.stringify(newItem));
     }
-    else{
-    const newItem = [...items, item];
-    setItems(newItem);
-    localStorage.setItem( 'ToDo' , JSON.stringify(newItem));
-    }
-  }
-  const deleteItem = (id)=>{
-    // setDialog("Are you sure you want to delete? sure you are sure ?", true);
-    const newItem = items.filter(item=> item.id !== id);
-    
+  };
+  const deleteItem2 = (id) => {
+    console.log(item.id);
+    const newItem = items.filter((item) => item.id !== id);
     setItems(newItem);
     localStorage.setItem("ToDo", JSON.stringify(newItem));
-  }
+    setItem(undefined);
+  };
+
+  const deleteItem = (id) => {
+    let array = items.filter((item) => item.id == id);
+    setItem(array[0]);
+  };
 
   const finishItem = (id) => {
-    setItems(items?.map(item => item.id === id ? { ...item, isDone: true } : item));
-  }
+    setItems(
+      items?.map((item) => (item.id === id ? { ...item, isDone: true } : item))
+    );
+  };
+  const displayDialog = (item) => {
+    item !== undefined ? setItem(item) : setItem(undefined);
+  };
 
   return (
     <div className="App">
       <div className="header">
-      <h1>To Do List</h1>
-      <div className="but">
-     <button className={currentPage==='add'? "active" : ""}
-      onClick={()=>setCurrentPage('add')}>Add Page</button>
-     <button className={currentPage==='view'? "active" : ""} 
-      onClick={()=>setCurrentPage('view') } >View Page</button>
-     </div>
-     </div>
-     {currentPage === 'add' && <AddItemPage onAdd={addItem}/>}
-     {currentPage === 'view' && <ViewItemPage items={items} onDelete={deleteItem} onFinish={finishItem}/>}
-     
+        <h1>To Do List</h1>
+        <div className="but">
+          <button
+            className={currentPage === "add" ? "active" : ""}
+            onClick={() => setCurrentPage("add")}
+          >
+            Add Page
+          </button>
+          <button
+            className={currentPage === "view" ? "active" : ""}
+            onClick={() => setCurrentPage("view")}
+          >
+            View Page
+          </button>
+        </div>
+      </div>
+      {currentPage === "add" && <AddItemPage onAdd={addItem} />}
+      {currentPage === "view" && (
+        <ViewItemPage
+          items={items}
+          displayDialog={displayDialog}
+          onDelete={deleteItem}
+          onFinish={finishItem}
+        />
+      )}
+      {item && (
+        <Dialog onDelete={deleteItem2} item={item} onPop={displayDialog} />
+      )}
     </div>
   );
 }
