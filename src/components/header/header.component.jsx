be842ac75d060a59e387a4423607e9e5feb62e83
zainabmanasrah <zainabmanasrah0@gmail.com>
2022-11-10 22:51:50 +0200

    commit

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,12 +1,14 @@
-import { Link } from 'react-router-dom';
+import { Link,useLocation } from 'react-router-dom';
 import './header.css'
 const Header = (props) => {
+  const location=useLocation();
   return (
     <div className="header">
       <h1>to do app</h1>
       <nav>
-        <Link to='/add'> Add</Link>
-        <Link to='/view'>
+        <Link to='/add'  className={location.pathname === '/add' ? 'active' : ''}> Add</Link>
+        {/* location.pathname.includes('view') */}
+       <Link to='/view' className={location.pathname=== '/view' ? 'active' : ''}>
 View
         </Link>
         {/* <button
